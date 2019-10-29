c5a752183c20e1432c43eae247b3928349b1e453
Administrator <Administrator@ZJZL-20190808DV>
2019-10-29 10:25:43 +0800

    2019-10-29 组长张晟第一次提交后台项目

---
+++ OrderNext/src/main/java/com/ordersys/OrderNextApplication.java
@@ -0,0 +1,13 @@
+package com.ordersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderNextApplication.class, args);
+	}
+
+}
